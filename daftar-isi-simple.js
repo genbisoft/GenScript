024e6152844b6f777ce24c620a3b849ca82d7499
Zizarun Namhar <genbisoft@users.noreply.github.com>
2019-02-12 16:27:54 +0700

    Update daftar-isi-simple.js

--- daftar-isi-simple.js
+++ daftar-isi-simple.js
@@ -1,22 +1,6 @@
 // ---------------------------------------------------
 // BLOGTOC
 // ---------------------------------------------------
-// BlogToc creates a clickable Table Of Contents for
-// Blogger Blogs.
-// It uses the JSON post feed, and create a ToC of it.
-// The ToC can be sorted by title or by date, both
-// ascending and descending, and can be filtered by
-// label.
-// ---------------------------------------------------
-// Author: Beautiful Beta
-// Url: http://beautifulbeta.blogspot.com
-// Version: 2
-// Date: 2007-04-12
-// ---------------------------------------------------
-// Modified by Aneesh 
-// www.bloggerplugins.org
-// Date : 02-08-2011
-// global arrays
 
    var postTitle = new Array();     // array of posttitles
    var postUrl = new Array();       // array of posturls
